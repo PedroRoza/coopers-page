475439ed36823a06cdb8f4493062e92e2f9f9832
Pedro Vinícius <pedrosilva.rosa@hotmail.com>
2025-05-27 19:37:24 -0300

    config env next cong

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
   images: {
     domains: ["https://coopers-page.vercel.app"], // Opcional: para imagens de outros domínios
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_TO: process.env.EMAIL_TO,
+  }
 };
 
 export default nextConfig;
